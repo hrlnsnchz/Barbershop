109336016bc7368197f92f45980700923f0ecdaf
hrlnsnchz <hrlnsnchz@gmail.com>
2021-06-20 16:08:46 -0500

    Sends a list of barbers in server response

--- barbershop/urls.py
+++ barbershop/urls.py
@@ -31,5 +31,4 @@ urlpatterns = [
     path('admin/', admin.site.urls),
     path('register', register_user),
     path('login', login_user),
-    path('api-auth', include('rest_framework.urls', namespace='rest_framework')),
 ]
