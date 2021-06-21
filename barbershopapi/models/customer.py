23e08368a60b4351996b9b78b430b53c2d110918
hrlnsnchz <hrlnsnchz@gmail.com>
2021-06-21 12:03:45 -0500

    Waitlist and other views have been set up with functioning GET request features. New users can also be registered from the app. Made changes to the customer model for this to work.

--- barbershopapi/models/customer.py
+++ barbershopapi/models/customer.py
@@ -1,8 +1,9 @@
 from django.db import models
-
+from django.db.models.deletion import CASCADE
+from django.contrib.auth.models import User
 
 
 class Customer(models.Model):
 
-    name = models.CharField(max_length=70)
+    user = models.OneToOneField(User, on_delete=models.CASCADE)
     phone = models.IntegerField()
